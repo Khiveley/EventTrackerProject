4b9f5a7772cfec55d9a0d8a53c4bb86594cae640
Khiveley <kenneth.hiveley@gmail.com>
2021-10-08 16:46:32 -0500

    Create boot project, add JPA project dependency

---
+++ AfterBoot/src/main/java/com/skilldistillery/afterboot/AfterBootApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.afterboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterBootApplication.class, args);
+	}
+
+}
---
+++ AfterBoot/src/test/java/com/skilldistillery/afterboot/AfterBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.afterboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
