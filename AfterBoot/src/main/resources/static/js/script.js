8c35f0c9a3e7a9143abee5b8993c9542a6b04eba
Khiveley <kenneth.hiveley@gmail.com>
2021-10-18 00:25:37 -0500

    Added Add and Delete methodologies

--- AfterBoot/src/main/resources/static/js/script.js
+++ AfterBoot/src/main/resources/static/js/script.js
@@ -18,8 +18,40 @@ function init(){
 event.preventDefault();
 getAllStudies();
 	});
-}
+	document.addStudyForm.addStudy.addEventListener('click', function(event){
+		event.preventDefault();
+		let studyForm = document.addStudyForm;
+		let newStudy = {
+			imageUrl: studyForm.imageUrl.value,
+			studyDate: studyForm.studyDate.value,
+			start: studyForm.start.value,
+			end: studyForm.start.value,
+			language: studyForm.language.value,
+			focus: studyForm.focus.value,
+			comment: studyForm.comment.value,
+			source: studyForm.source.value,
+			pages: studyForm.pages.value
+		};
+		console.log(newStudy);
+		addStudy(newStudy);
+	});
+	document.deleteStudyForm.deleteStudy.addEventListener('click', function(event){
+		event.preventDefault();
+		let studyId = document.deleteStudyForm.studyId.value;
+		if(!isNaN(studyId) && studyId > 0){
+			deleteStudy(studyId);
+		} else {
+			console.log("An error occurred.");
+			alert("Invalid Entry/Number. Try Again.");
+		}
+	});
+	
+};
 
+var updateStudyForm = document.getElementById('updateStudyForm');
+console.log(updateStudyForm);
+
+var displayStudySessions = document.getElementById('displayStudySessions');
 function getAllStudies(){
 	let xhr = new XMLHttpRequest();
 	console.log('xhr.readyState =' + xhr.readyState);
@@ -62,7 +94,7 @@ function getStudy(studyId){
 	xhr.send();
 	console.log('After send: xhr.readyState =' + xhr.readyState);
 }
-
+// Get a study
 function displayStudy(study){
 
 	let studySessions = document.getElementById('studySessions');
@@ -162,7 +194,12 @@ img.src = study.imageUrl;
 		let br = document.createElement('br');
 	studySessions.appendChild(br);
 }
-
+var itemClick = function(e){
+console.log('in itemClick');
+e.preventDefault();
+updateStudyForm.imageUrl.value = this.firstElementChild.textContent;
+};
+// GET list of Studies
 function displayAllStudies(studies){
 
 	let studySessions = document.getElementById('studySessions');
@@ -259,11 +296,94 @@ img.src = study.imageUrl;
 		td = document.createElement('td');
 		td.textContent = study.source;
 		tr.appendChild(td);
-		let hr = document.createElement('hr');
-		studySessions.appendChild(hr);
+		// let hr = document.createElement('hr');
+		// studySessions.appendChild(hr);
+		tr.addEventListener('click', itemClick);
 		tbod.appendChild(tr);
 	
 	
 	
+	}
 }
+
+// Add a Study
+function addStudy(newStudy) {
+	console.log("Start of addStudy");
+let xhr = new XMLHttpRequest();
+xhr.open('POST', 'api/studies');
+xhr.onreadystatechange = function(){
+	if(xhr.status === 4){
+		if(xhr.status === 201){
+			let study = JSON.parse(xhr.responseText);
+			displayStudy(study);
+			console.log("in addStudy");
+		}
+	 else if(xhr.status >= 400 && xhr.status < 500){
+		console.error('Something went wrong.');
+		console.log(xhr.status);
+		alert('Something went wrong.');
+	 }
+	}
 }
+xhr.setRequestHeader('Content-type', 'application/json');
+xhr.send(JSON.stringify(newStudy));
+}
+// Delete a Study
+function deleteStudy(studyId){
+	console.log("Start of deleteStudy");
+	let xhr = new XMLHttpRequest();
+	xhr.open('DELETE', 'api/studies/' + studyId);
+	console.log('xhr.readyState =' + xhr.readyState);
+	xhr.onreadystatechange = function(){
+		console.log('xhr.readyState =' + xhr.readyState);
+		if (xhr.readyState === 4){
+			if (xhr.status === 204){
+				if(alert("The study session has been deleted.")){}
+			else window.location.reload();
+
+			}
+			else if(xhr.status === 404){
+				console.error("No study session found");
+				alert("The study session you are looking for was not found.  Try again.");
+			}
+		}
+	}
+	xhr.send();
+}
+
+// Update a Study
+function updateStudy(studyId){
+	console.log("In updateStudy");
+	var xhr = new XMLHttpRequest();
+	xhr.open('PUT', 'api/studies/' + studyId, true);
+
+	xhr.setRequestHeader('Content-type', 'application/json');
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4){
+			if (xhr.status === 201){
+				getstudy();
+				console.log('Study updated');
+				alert('Study updated');
+			} else {
+				console.log("Study not updated");
+				console.log(xhr.status);
+				alert('Study not updated.  Try again')
+			}
+
+		}
+		let updateStudy = {
+			imageUrl: updateStudyForm.imageUrl.value,
+			studyDate: updateStudyForm.studyDate.value,
+			start: updateStudyForm.start.value,
+			end: updateStudyForm.start.value,
+			language: updateStudyForm.language.value,
+			focus: updateStudyForm.focus.value,
+			comment: updateStudyForm.comment.value,
+			source: updateStudyForm.source.value,
+			pages: updateStudyForm.pages.value
+		}
+		xhr.send(JSON.stringify(updateStudy));
+	}
+}
+
+
